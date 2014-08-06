ee664f753854fd9d0f036268e1522b11df8b6d4c
unenglishable <unenglishable@gmail.com>
2014-08-05 17:06:57 -1000

    epochPost stream now uses epochMap for created time

--- import/smf/smfToEpochStream/epochPostStream.js
+++ import/smf/smfToEpochStream/epochPostStream.js
@@ -12,7 +12,8 @@ EpochPostStream.prototype.createPostStream = function (err, oldThreadId, newThre
   var table = 'smf_messages';
   var tableMap = {
     subject : 'title',
-    body : 'body'
+    body : 'body',
+    posterTime: 'created_at'
   }
 
   var smfMap = {
@@ -23,7 +24,7 @@ EpochPostStream.prototype.createPostStream = function (err, oldThreadId, newThre
   var tr = through2.obj(function (row, enc, cb) {
     var obj = epochMap.remapObject(row, tableMap);
     // Handling for created_at
-    obj['created_at'] = row.posterTime*1000;
+    obj['created_at'] = row.posterTime;
     obj['thread_id'] = newThreadId;
     var smfObject = epochMap.remapObject(row, smfMap);
     obj['smf'] = smfObject;
