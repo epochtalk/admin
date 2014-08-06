53ef21bd46156a6a39c23b64500f768eb89c4c20
unenglishable <unenglishable@gmail.com>
2014-08-05 17:06:03 -1000

    epochMap now checks for empty strings and discards them

--- import/smf/smfToEpochStream/epochMap.js
+++ import/smf/smfToEpochStream/epochMap.js
@@ -2,7 +2,7 @@ exports.remapObject = function (oldObject, map) {
   var obj = {};
   for (oldKey in map) {
     var newKey = map[oldKey];
-    obj[newKey] = oldObject[oldKey];
+    obj[newKey] = oldObject[oldKey] ? oldObject[oldKey] : undefined;
   }
   return obj;
 }
