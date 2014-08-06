725b583ccdeadb93845c98c2d5bc53f88fcbc575
unenglishable <unenglishable@gmail.com>
2014-08-05 17:08:12 -1000

    update Thread stream to post the first post
    uses the method in mysqlQuerier for getting an smf_message with the
    same ID_MSG as the smf_topic's ID_FIRST_MSG
    
    changed config to use 100 simultaneous pool connections
    if the threshold is set too low, it can create a problem with
    asynchronous bottlenecking.  calls to mysql might depend on eachother
    and may become locked in waiting if there are not enough available
    connections.

--- import/smf/smfToEpochStream/epochThreadStream.js
+++ import/smf/smfToEpochStream/epochThreadStream.js
@@ -10,6 +10,11 @@ var EpochThreadStream = module.exports = function EpochThreadStream(mQ) {
 
 EpochThreadStream.prototype.createThreadStream = function (err, oldBoardId, newBoardId) {
   var table = 'smf_topics';
+  var tableMap = {
+    subject : 'title',
+    body: 'body',
+    posterTime: 'created_at'
+  }
 
   var smfMap = {
     ID_TOPIC : 'thread_id',
@@ -17,12 +22,17 @@ EpochThreadStream.prototype.createThreadStream = function (err, oldBoardId, newB
   }
 
   var rowStreamWhere = this.mQ.createRowStreamWhere(null, table, { ID_BOARD : oldBoardId});
+  var mQ = this.mQ;
   var tr = through2.obj(function (row, enc, cb) {
-    var obj = { board_id : newBoardId };
-    var smfObject = epochMap.remapObject(row, smfMap);
-    obj['smf'] = smfObject;
-    this.push(obj);
-    return cb();
+    mQ.getRowsWhere(null, 'smf_messages', { ID_MSG : row.ID_FIRST_MSG }, function (err, firstPost) {
+      firstPost = firstPost[0];
+      var obj = epochMap.remapObject(firstPost, tableMap);
+      obj['board_id'] = newBoardId;
+      var smfObject = epochMap.remapObject(row, smfMap);
+      obj['smf'] = smfObject;
+      tr.push(obj);
+      return cb();
+    });
   });
   threadStream = rowStreamWhere.pipe(tr);
 
