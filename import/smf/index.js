3ebf613d8486a760c4d07d7b4102f3002a562d29
unenglishable <unenglishable@gmail.com>
2014-08-15 17:11:17 -1000

    update to use new smf-import name

--- import/smf/index.js
+++ import/smf/index.js
@@ -1,7 +1,7 @@
-var smfImporter = require('smf-importer');
+var smfImport = require('smf-import');
 module.exports = {
   run: function(args, cb) {
          console.log('SMF Importer with args: ' + JSON.stringify(args));
-         smfImporter(args.debug, cb);
+         smfImport(args.debug, cb);
        }
 }
